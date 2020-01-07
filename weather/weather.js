edae3c1e5ee0c6f72e7de7464b5020fc407cdf10
Nwokolo Chilo <despeakableme@gmail.com>
2020-01-07 16:11:37 +0100

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,5 +1,3 @@
-// forecast.io api key bfe2065cff9b0bc0dcda578b8bd5bd5a
-// https://maps.google.com/maps/api/geocode/json?address=23%20Karimu%20Kotun%20street&key=AIzaSyAGZQMgUL7i9TjRNl3z8vzL3cNPj-VkZ_Y
 const request = require('request');
 
 getWeather = (lat, lng, callback) => {
